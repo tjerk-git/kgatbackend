9bf856884561768ab0de2642f2a133eac081a3a5
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-19 15:58:45 +0200

    ok

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST'],
 
-    'allowed_origins' => ['http://127.0.0.1:5500/'],
+    'allowed_origins' => ['http://127.0.0.1:5500/',  'https://kijkgat.com'],
 
     'allowed_origins_patterns' => [],
 
