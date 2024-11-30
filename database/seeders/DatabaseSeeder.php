436ecc726a035e9b1224a1876508fdf37ee0c286
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-11-30 20:22:52 +0100

    ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Tjerk',
+            'email' => 'tjerk.dijkstra@icloud.com',
+            'password' => bcrypt('12345678'),
         ]);
     }
 }
